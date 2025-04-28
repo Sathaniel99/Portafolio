dd2cc7a68d3db398b0d68999888c73bfbb686247
Adán Luis Quincoces Escalona <adanlqe@gmail.com>
2025-04-28 14:51:28 -0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "https://sathaniel99.github.io/portfolio/",
+  base : "https://sathaniel99.github.io/Portafolio/",
 })
