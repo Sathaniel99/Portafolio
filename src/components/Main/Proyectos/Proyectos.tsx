ccd1d48f57b3b9551b421add4f3ff79401bb6f60
Adán Luis Quincoces Escalonas <129526298+Sathaniel99@users.noreply.github.com>
2025-12-16 11:05:42 -0500

    Update Proyectos.tsx

--- src/components/Main/Proyectos/Proyectos.tsx
+++ src/components/Main/Proyectos/Proyectos.tsx
@@ -17,7 +17,7 @@ export const Proyectos = () => {
     useEffect(() => {
         const fetchRepos = async () => {
             try {
-                const response = await axios.get("repos.json");
+                const response = await axios.get("https://api.github.com/users/Sathaniel99/repos?type=owner");
                 setRepos(response.data);
             } catch (err) {
                 setError(
